251e38fcf0da91613760b9ef64f3dbb80c291037
Abdurrahman Qureshi <scythethroughtheheart05@gmail.com>
2023-09-29 18:48:16 +0530

    claer left

--- src/components/Game.js
+++ src/components/Game.js
@@ -5,6 +5,8 @@ import "../index.css";
 const Game = () => {
     const [dine, setDine] = useState(true);
     const [blocks, setBlocks] = useState(Array(9).fill(""));
+    const [xWins , setXWins] = useState(0);
+    const [oWins , setOWins] = useState(0);
 
     const blockClickHandler = (index) => {
         if (blocks[index] === "") {
@@ -14,36 +16,52 @@ const Game = () => {
 
             if (newBlocks[0] === "X" && newBlocks[1] === "X" && newBlocks[2] === "X") {
                 alert("X wins");
+                setXWins(xWins+1);
             } else if (newBlocks[0] === "O" && newBlocks[1] === "O" && newBlocks[2] === "O") {
-                alert("O wins");
+                alert("X wins");
+                setOWins(oWins+1);
             } else if (newBlocks[3] === "X" && newBlocks[4] === "X" && newBlocks[5] === "X") {
                 alert("X wins");
+                setXWins(xWins+1);
             } else if (newBlocks[3] === "O" && newBlocks[4] === "O" && newBlocks[5] === "O") {
-                alert("O wins");
+                alert("X wins");
+                setOWins(oWins+1);
             } else if (newBlocks[6] === "X" && newBlocks[7] === "X" && newBlocks[8] === "X") {
                 alert("X wins");
+                setXWins(xWins+1);
             } else if (newBlocks[6] === "O" && newBlocks[7] === "O" && newBlocks[8] === "O") {
-                alert("O wins");
+                alert("X wins");
+                setOWins(oWins+1);
             } else if (newBlocks[0] === "X" && newBlocks[3] === "X" && newBlocks[6] === "X") {
                 alert("X wins");
+                setXWins(xWins+1);
             } else if (newBlocks[0] === "O" && newBlocks[3] === "O" && newBlocks[6] === "O") {
-                alert("O wins");
+                alert("X wins");
+                setOWins(oWins+1);
             } else if (newBlocks[1] === "X" && newBlocks[4] === "X" && newBlocks[7] === "X") {
                 alert("X wins");
+                setXWins(xWins+1);
             } else if (newBlocks[1] === "O" && newBlocks[4] === "O" && newBlocks[7] === "O") {
-                alert("O wins");
+                alert("X wins");
+                setOWins(oWins+1);
             } else if (newBlocks[5] === "X" && newBlocks[5] === "X" && newBlocks[8] === "X") {
                 alert("X wins");
+                setXWins(xWins+1);
             } else if (newBlocks[5] === "O" && newBlocks[5] === "O" && newBlocks[8] === "O") {
-                alert("O wins");
+                alert("X wins");
+                setOWins(oWins+1);
             } else if (newBlocks[0] === "X" && newBlocks[4] === "X" && newBlocks[8] === "X") {
                 alert("X wins");
+                setXWins(xWins+1);
             } else if (newBlocks[0] === "O" && newBlocks[4] === "O" && newBlocks[8] === "O") {
-                alert("O wins");
+                alert("X wins");
+                setOWins(oWins+1);
             } else if (newBlocks[2] === "X" && newBlocks[4] === "X" && newBlocks[6] === "X") {
                 alert("X wins");
+                setXWins(xWins+1);
             } else if (newBlocks[2] === "O" && newBlocks[4] === "O" && newBlocks[6] === "O") {
-                alert("O wins");
+                alert("X wins");
+                setOWins(oWins+1);
             }
             setDine(!dine);
         }
@@ -52,7 +70,11 @@ const Game = () => {
     return (
         <>
             <StyledHeader className={dine ? "red" : "blue"}>
-                <StyledHeading className={dine ? "blueH" : "redH"}>TIC-TAC-TOE</StyledHeading>
+                <StyledNav>
+                    <StyledPlayer className={dine ? "blueH" : "redH"}>Player X wins: {xWins}</StyledPlayer>
+                    <StyledHeading className={dine ? "blueH" : "redH"}>TIC-TAC-TOE</StyledHeading>
+                    <StyledPlayer className={dine ? "blueH" : "redH"}>Player O wins: {oWins}</StyledPlayer>
+                </StyledNav>
                 <StyledBoard>
                     {blocks.map((content, index) => (
                         <StyledBlock className={dine ? "red" : "blue"} key={index}
@@ -61,6 +83,7 @@ const Game = () => {
                         </StyledBlock>
                     ))}
                 </StyledBoard>
+                <StyledClear className={dine ? "blueB" : "redB"}>Restart</StyledClear>
             </StyledHeader>
         </>
     );
@@ -75,15 +98,26 @@ const StyledHeader = styled.div`
   align-items: center;
 `;
 
+const StyledNav = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 const StyledHeading = styled.h1`
   letter-spacing: 6px;
-  padding-bottom: 3rem;
+  padding: 0 7rem 3rem;  
   font-size: 5rem;
 `;
+const StyledPlayer = styled.div`
+  padding-bottom: 3rem;
+  font-size: 3rem;
+`;
+
 const StyledBoard = styled.div`
   display: grid;
-  grid-template-columns: 30% 30% 30%;
-  grid-gap: 20px;
+  grid-template-columns: 7rem 7rem 7rem;
+  grid-gap: 1rem;
 `;
 
 const StyledBlock = styled.div`
@@ -94,8 +128,27 @@ const StyledBlock = styled.div`
   background-color: #666666;
   border: 2px solid black;
   border-radius: 10px;
-  width: 10rem;
-  height: 10rem;
+  width: 7rem;
+  height: 7rem;
+  transition: 0.5s all ease;
+
+  &:hover {
+    cursor: pointer;
+    transform: scale(0.95);
+  }
+`;
+
+const StyledClear = styled.div`
+  margin-top: 3rem;
+  font-size: 1.5rem;
+  letter-spacing: 5px;
+  border: 2px solid black;
+  border-radius: 10px;
+  width: 15rem;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   transition: 0.5s all ease;
 
   &:hover {
